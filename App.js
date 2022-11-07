592e07a00f8b1ef9b06293999e4b87e75727c266
lindsayPeeters <lindsay.99.peeters@gmail.com>
2022-11-07 19:38:46 +0100

    title

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading1}>Bookshop</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 56,
+    padding: 16,
+    backgroundColor: '#fff',  
+  },
+  heading1: {
+    fontSize: 32,
+    fontWeight: '600',
   },
 });
